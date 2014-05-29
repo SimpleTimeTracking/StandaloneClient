9a45f44f38db7a66882f984486e58a409a207a17
dante <dennis.waldherr@gmx.de>
2014-05-29 13:43:42 +0200

    * Ignoring "build" directory now. * Changed ItemWriter interface * Added guice and javafx dependencies

--- src/main/java/org/stt/persistence/ItemWriter.java
+++ src/main/java/org/stt/persistence/ItemWriter.java
@@ -4,21 +4,18 @@ import java.util.Calendar;
 
 public interface ItemWriter {
 	/**
-	 * Inserts the item at the given date and time with the given duration.
-	 * <p>
-	 * If the new time block falls between an existing one, the old "task" will
-	 * be split up, ending with the start of the new item and starting again
-	 * after the duration has passed.
-	 * </p>
-	 * <p>
-	 * <i>To Be Designed</i> If the time block completely overlaps a previous
-	 * block, it will be split up: It will end with the start of the first
-	 * overlapping block, and begin once again with the end of the last
-	 * overlapping block.
-	 * </p>
+	 * Inserts a new interval. If existing intervals are completely covered,
+	 * they will be removed, otherwise they will be cropped as needed.
 	 */
-	void writeItemWithDurationAt(Calendar dateAndTime, Duration duration,
-			String description);
+	void writeItemWithDurationAtAndOverwriteCoveredIntervals(
+			Calendar dateAndTime, Duration duration, String description);
+
+	/**
+	 * Inserts a new interval. If existing intervals are completely covered,
+	 * they will be retained, otherwise they will be cropped as needed.
+	 */
+	void writeItemWithDurationAndAndRetainCoveredIntervals(
+			Calendar dateAndTime, Duration duration, String description);
 
 	/**
 	 * Inserts the item at the given date and time. The item before and after
