038a10837b592058667e176e5ad333c55d648101
bottomquark <bottomquark@users.noreply.github.com>
2014-06-13 21:12:32 +0200

    allow environment variables in properties

--- src/main/java/org/stt/Configuration.java
+++ src/main/java/org/stt/Configuration.java
@@ -4,7 +4,15 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Simple configuration mechanism with fallback values.
+ * 
+ * Resolves environment variables like HOME if enclosed in $ signs like so:
+ * $HOME$/.stt
+ */
 public class Configuration {
 
 	private final File propertiesFile = new File(
@@ -13,6 +21,9 @@ public class Configuration {
 	private static Configuration instance = null;
 
 	private Properties loadedProps;
+
+	private static final Pattern ENV_PATTERN = Pattern
+			.compile(".*\\$(.*)\\$.*");
 
 	private Configuration() {
 		loadedProps = new Properties();
@@ -31,20 +42,32 @@ public class Configuration {
 	}
 
 	public File getSttFile() {
-		String sttFile = loadedProps.getProperty("sttFile",
-				System.getProperty("user.home") + "/.stt");
+		String sttFile = getPropertiesReplaced("sttFile", "$HOME$/.stt");
 		return new File(sttFile);
 	}
 
 	public File getTiFile() {
-		String tiFile = loadedProps.getProperty("tiFile",
-				System.getProperty("user.home") + "/.ti-sheet");
+		String tiFile = getPropertiesReplaced("tiFile", "$HOME$/.ti-sheet");
 		return new File(tiFile);
 	}
 
 	public File getTiCurrentFile() {
-		String tiFile = loadedProps.getProperty("tiCurrentFile",
-				System.getProperty("user.home") + "/.ti-sheet-current");
+		String tiFile = getPropertiesReplaced("tiCurrentFile",
+				"$HOME$/.ti-sheet-current");
 		return new File(tiFile);
+	}
+
+	private String getPropertiesReplaced(String propName, String fallback) {
+		String theProperty = loadedProps.getProperty(propName, fallback);
+		Matcher envMatcher = ENV_PATTERN.matcher(theProperty);
+		if (envMatcher.find()) {
+			String group = envMatcher.group(1);
+			String getenv = System.getenv(group);
+			if (getenv != null) {
+				theProperty = theProperty.replace("$" + group + "$", getenv);
+			}
+		}
+		return theProperty;
+
 	}
 }
