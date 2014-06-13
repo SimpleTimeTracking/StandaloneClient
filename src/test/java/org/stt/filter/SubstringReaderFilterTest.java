61fbbca6791e6aab5cfb41727c987fa84f2539db
bottomquark <bottomquark@users.noreply.github.com>
2014-06-13 22:15:23 +0200

    test for substring reader filter

---
+++ src/test/java/org/stt/filter/SubstringReaderFilterTest.java
@@ -0,0 +1,61 @@
+package org.stt.filter;
+
+import java.io.IOException;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.stt.model.TimeTrackingItem;
+import org.stt.persistence.ItemReader;
+
+import com.google.common.base.Optional;
+
+public class SubstringReaderFilterTest {
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void itemContainingSubstringGetsFound() throws IOException {
+
+		// GIVEN
+		ItemReader readerMock = Mockito.mock(ItemReader.class);
+		Mockito.when(readerMock.read())
+				.thenReturn(
+						Optional.of(new TimeTrackingItem("the comment",
+								DateTime.now())),
+						Optional.<TimeTrackingItem> absent());
+
+		SubstringReaderFilter filter = new SubstringReaderFilter(readerMock,
+				"comment");
+
+		// WHEN
+		Optional<TimeTrackingItem> read = filter.read();
+		filter.close();
+
+		// THEN
+		Assert.assertEquals("the comment", read.get().getComment().get());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void itemNotContainingSubstringGetsNotFound() throws IOException {
+
+		// GIVEN
+		ItemReader readerMock = Mockito.mock(ItemReader.class);
+		Mockito.when(readerMock.read())
+				.thenReturn(
+						Optional.of(new TimeTrackingItem("the comment",
+								DateTime.now())),
+						Optional.<TimeTrackingItem> absent());
+
+		SubstringReaderFilter filter = new SubstringReaderFilter(readerMock,
+				"not there");
+
+		// WHEN
+		Optional<TimeTrackingItem> read = filter.read();
+		filter.close();
+
+		// THEN
+		Assert.assertEquals(Optional.absent(), read);
+	}
+}
