ef67010b1b9b5d52f7138e21083e746948cd884f
dante <dennis.waldherr@gmx.de>
2014-06-20 11:28:58 +0200

    StartDateReaderFilter is now filtering the interval [start,end)

--- src/main/java/org/stt/filter/StartDateReaderFilter.java
+++ src/main/java/org/stt/filter/StartDateReaderFilter.java
@@ -9,14 +9,14 @@ import org.stt.persistence.ItemReader;
 import com.google.common.base.Optional;
 
 /**
- * Reads from the given reader but only returns items where from <= start date
- * >= to
+ * Reads from the given reader but only returns items where from <= start date <
+ * to.
  */
 public class StartDateReaderFilter implements ItemReader {
 
-	private ItemReader reader;
-	private DateTime from;
-	private DateTime to;
+	private final ItemReader reader;
+	private final DateTime from;
+	private final DateTime to;
 
 	public StartDateReaderFilter(ItemReader reader, DateTime from, DateTime to) {
 		this.reader = reader;
@@ -30,7 +30,7 @@ public class StartDateReaderFilter implements ItemReader {
 		Optional<TimeTrackingItem> item;
 		while ((item = reader.read()).isPresent()) {
 			DateTime start = item.get().getStart();
-			if (!start.isAfter(to) && !start.isBefore(from)) {
+			if (to.isAfter(start) && !start.isBefore(from)) {
 				return item;
 			}
 		}
---
+++ src/test/java/org/stt/filter/StartDateReaderFilterTest.java
@@ -0,0 +1,59 @@
+package org.stt.filter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito.BDDMyOngoingStubbing;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.stt.model.TimeTrackingItem;
+import org.stt.persistence.IOUtil;
+import org.stt.persistence.ItemReader;
+
+import com.google.common.base.Optional;
+
+public class StartDateReaderFilterTest {
+	private StartDateReaderFilter sut;
+	@Mock
+	private ItemReader reader;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void shouldContainStartInclusiveAndEndExclusive() throws IOException {
+		// GIVEN
+		DateTime from = new DateTime(2000, 1, 1, 0, 0);
+		DateTime to = new DateTime(2000, 1, 2, 0, 0);
+		givenReaderReturnsItemsFor(from, to);
+		sut = new StartDateReaderFilter(reader, from, to);
+
+		// WHEN
+		Collection<TimeTrackingItem> result = IOUtil.readAll(sut);
+
+		// THEN
+		assertThat(result, IsCollectionWithSize.hasSize(1));
+		assertThat(result.iterator().next().getStart(), is(from));
+	}
+
+	private void givenReaderReturnsItemsFor(DateTime... starts) {
+		BDDMyOngoingStubbing<Optional<TimeTrackingItem>> stubbing = given(reader
+				.read());
+		for (DateTime start : starts) {
+			stubbing = stubbing.willReturn(Optional.of(new TimeTrackingItem("",
+					start)));
+		}
+		stubbing.willReturn(Optional.<TimeTrackingItem> absent());
+	}
+
+}
