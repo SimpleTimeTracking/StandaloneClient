6798f4b82794afd2d7fdb97789913a2ac95ce7ed
bottomquark <bottomquark@users.noreply.github.com>
2014-07-08 19:31:26 +0200

    failing test for "since" report parameter

--- src/test/java/org/stt/cli/ReportPrinterTest.java
+++ src/test/java/org/stt/cli/ReportPrinterTest.java
@@ -1,23 +1,27 @@
 package org.stt.cli;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.stt.Configuration;
 import org.stt.ItemReaderTestHelper;
+import org.stt.model.TimeTrackingItem;
 import org.stt.persistence.ItemReader;
 import org.stt.persistence.ItemReaderProvider;
 import org.stt.reporting.WorkingtimeItemProvider;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import static org.junit.Assert.assertThat;
+
+import static org.mockito.BDDMockito.given;
 
 public class ReportPrinterTest {
 	private ReportPrinter sut;
@@ -36,6 +40,8 @@ public class ReportPrinterTest {
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+
+		given(configuration.getCliReportingWidth()).willReturn(120);
 		given(readFrom.provideReader()).willReturn(itemReader);
 		sut = new ReportPrinter(readFrom, configuration,
 				workingtimeItemProvider);
@@ -46,12 +52,16 @@ public class ReportPrinterTest {
 		// GIVEN
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(out, true, "UTF8");
-		ItemReaderTestHelper.givenReaderReturns(itemReader);
+		ItemReaderTestHelper.givenReaderReturns(itemReader,
+				new TimeTrackingItem("comment", DateTime.now().minusHours(2),
+						DateTime.now().minusHours(1)));
 
 		// WHEN
 		sut.report(Collections.singleton("since 2013-01-01"), printStream);
 
 		// THEN
+		String result = new String(out.toByteArray(), "UTF8");
+		assertThat(result, containsString("comment"));
 	}
 
 	@Test
@@ -74,12 +84,14 @@ public class ReportPrinterTest {
 		// GIVEN
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(out, true, "UTF8");
-		ItemReaderTestHelper.givenReaderReturns(itemReader);
+		ItemReaderTestHelper.givenReaderReturns(itemReader,
+				new TimeTrackingItem("comment blub and stuff", DateTime.now()));
 
 		// WHEN
 		sut.report(Collections.singleton("blub"), printStream);
 
 		// THEN
-
+		String result = new String(out.toByteArray(), "UTF8");
+		assertThat(result, containsString("comment blub"));
 	}
 }
