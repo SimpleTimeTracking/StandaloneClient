45ed6dcc8916eb76a78a409c94b5073215c60e9c
Blizzard26 <alanz@gmx.de>
2017-03-17 18:17:31 +0100

    Added JiraExpansionProvider
    Added JiraExpansionProvider to provide expansions based on Jira Issue IDs when entering Tasks

---
+++ src/main/java/org/stt/text/JiraExpansionProvider.java
@@ -0,0 +1,55 @@
+package org.stt.text;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.stt.connector.jira.JiraConnector;
+import org.stt.connector.jira.JiraConnectorException;
+
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.google.common.base.Optional;
+import com.google.inject.Inject;
+
+public class JiraExpansionProvider implements ExpansionProvider {
+
+    private static final Logger LOG = Logger.getLogger(JiraExpansionProvider.class
+            .getName());
+	
+	private JiraConnector jiraConnector;
+
+	@Inject
+	public JiraExpansionProvider(JiraConnector connector) {
+		this.jiraConnector = checkNotNull(connector);
+	}
+	
+	@Override
+	public List<String> getPossibleExpansions(String text) {
+		String queryText = text.trim();
+		int spaceIndex = queryText.lastIndexOf(' ');
+		if (spaceIndex > 0)
+		{
+			queryText = text.substring(spaceIndex, text.length()).trim();
+		}
+		
+		Optional<Issue> issue;
+		try {
+			issue = jiraConnector.getIssue(queryText);
+		} catch (JiraConnectorException e) {
+			LOG.severe(e.getLocalizedMessage());
+			return Collections.emptyList();
+		}
+		
+		List<String> expansions = new ArrayList<>();
+		if (issue.isPresent())
+		{
+			expansions.add(": " + issue.get().getSummary());
+		}
+		
+		return expansions;
+	}
+
+}
