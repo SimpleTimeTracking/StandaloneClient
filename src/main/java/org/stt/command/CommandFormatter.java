cb9360e7b53a63efcd9c34748e1d84bae24f3041
Bytekeeper <therealdante@gmail.com>
2017-06-19 19:38:33 +0200

    Change to non-deprecated class

--- src/main/java/org/stt/command/CommandFormatter.java
+++ src/main/java/org/stt/command/CommandFormatter.java
@@ -1,9 +1,7 @@
 package org.stt.command;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.Interval;
 import org.stt.grammar.EnglishCommandsLexer;
 import org.stt.grammar.EnglishCommandsParser;
 import org.stt.model.TimeTrackingItem;
@@ -54,18 +52,61 @@ public class CommandFormatter {
     /**
      * Use case insensitive lookaheads but leave case of tokens.
      */
-    private static class CaseInsensitiveInputStream extends ANTLRInputStream {
-        protected CaseInsensitiveInputStream(String input) {
-            super(input);
+    private static class CaseInsensitiveInputStream implements CharStream {
+
+        private final CodePointCharStream delegate;
+
+        CaseInsensitiveInputStream(String input) {
+            delegate = CharStreams.fromString(input);
+        }
+
+        @Override
+        public void consume() {
+            delegate.consume();
         }
 
         @Override
         public int LA(int i) {
-            int la = super.LA(i);
+            int la = delegate.LA(i);
             if (Character.isAlphabetic(la)) {
                 return Character.toLowerCase(la);
             }
             return la;
+        }
+
+        @Override
+        public int mark() {
+            return delegate.mark();
+        }
+
+        @Override
+        public void release(int marker) {
+            delegate.release(marker);
+        }
+
+        @Override
+        public int index() {
+            return delegate.index();
+        }
+
+        @Override
+        public void seek(int index) {
+            delegate.seek(index);
+        }
+
+        @Override
+        public int size() {
+            return delegate.size();
+        }
+
+        @Override
+        public String getSourceName() {
+            return delegate.getSourceName();
+        }
+
+        @Override
+        public String getText(Interval interval) {
+            return delegate.getText(interval);
         }
     }
 
