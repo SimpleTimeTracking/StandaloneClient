238f7b0477051a42d0f6dc672b26b9196c525edc
Jens Häderer <jens.haederer@actico.com>
2024-01-31 20:50:00 +0100

    Add error handling in case request for Issue is not successful

--- src/main/kotlin/org/stt/connector/jira/Exceptions.kt
+++ src/main/kotlin/org/stt/connector/jira/Exceptions.kt
@@ -1,7 +1,8 @@
 package org.stt.connector.jira
 
 class Exceptions(message: String, cause: Throwable) : Exception(message, cause)
-class InvalidCredentialsException(message: String, cause: Throwable) : Exception(message, cause)
-class IssueDoesNotExistException(message: String, cause: Throwable) : Exception(message, cause)
+class InvalidCredentialsException(message: String) : Exception(message)
+class IssueDoesNotExistException(message: String) : Exception(message)
+class AccessDeniedException(message: String) : Exception(message)
 class JiraConnectorException(errorMessage: String, cause: Exception) : RuntimeException(errorMessage, cause)
 
--- src/main/kotlin/org/stt/connector/jira/JiraClient.kt
+++ src/main/kotlin/org/stt/connector/jira/JiraClient.kt
@@ -20,7 +20,7 @@ class JiraClient(private var username: String?, private var password: String?, p
     }
 
 
-    fun getIssue(issueKey: String): Issue {
+    fun getIssue(issueKey: String): Issue? {
         val jiraUrl = getJiraUrl()
         val request = HttpRequest.newBuilder()
                 .uri(URI.create("$jiraUrl$restApiV3/issue/$issueKey?fields=summary"))
@@ -30,18 +30,28 @@ class JiraClient(private var username: String?, private var password: String?, p
                 .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(("$username:$password").toByteArray()))
                 .build()
         this.restClient?.send(request, HttpResponse.BodyHandlers.ofString())?.let {
-            if(it.statusCode() == 200) {
-                val deserializedAsMap = JsonIterator.deserialize(it.body(), Map::class.java)
-                val fields = deserializedAsMap["fields"] as Map<String, String>
-                return Issue(fields["summary"])
-            }
+            if (it.statusCode() == 200) {
+            val deserializedAsMap = JsonIterator.deserialize(it.body(), Map::class.java)
+            val fields = deserializedAsMap["fields"] as Map<String, String>
+            return Issue(fields["summary"])
+          }
+          handleHttpError(it, issueKey)
         }
-                return Issue()
-
-
+        return null
     }
 
-    private fun getJiraUrl(): String {
+  private fun handleHttpError(response: HttpResponse<String>, issueKey: String) {
+    val httpStatusCode = response.statusCode()
+    val body = response.body()
+    when (httpStatusCode) {
+      401 -> throw AccessDeniedException("You don't have permission to see $issueKey.")
+      403 -> throw InvalidCredentialsException("Please check your Jira username/password.")
+      404 -> throw IssueDoesNotExistException("Couldn't find issue $issueKey. Cause: $body")
+      in 400..500 -> throw Exception("Error while retrieving issue $issueKey: $httpStatusCode: $body")
+    }
+  }
+
+  private fun getJiraUrl(): String {
         val jiraUrlAsCharArray = this.jiraUrl.toCharArray()
         if(jiraUrlAsCharArray[jiraUrlAsCharArray.size - 1]  == '/') {
             return this.jiraUrl
